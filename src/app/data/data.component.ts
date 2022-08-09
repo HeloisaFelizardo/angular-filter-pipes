4afb37ba83ab03051814536d9e2c274458682a57
Heloisa Felizardo <helocmp@yahoo.com.br>
2022-08-09 11:31:38 -0300

    first commit

---
+++ src/app/data/data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.css']
+})
+export class DataComponent {
+
+  hoje = new Date(1989, 2, 19);
+}
